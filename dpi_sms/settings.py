8adc3af1a3111bb964c320198334526ba9f67a8d
Bala prajapatil <balapp143@gmail.com>
2021-04-01 18:51:19 +0530

    asfd

--- dpi_sms/settings.py
+++ dpi_sms/settings.py
@@ -156,10 +156,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT =  '/home/xarxa-15/www/college/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
 
 
